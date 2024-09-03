5763038f759ab674b6ea4da6042dcbf8985e8903
Federica <federicapaganica@icloud.com>
2024-09-03 10:53:10 +0200

    Aggiornata connessione al DB

---
+++ src/config/database.ts
@@ -0,0 +1,53 @@
+/**
+ * @file database.ts
+ * @description File per la configurazione del DB con Sequelize
+ */
+
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// TODO
+const dbName = process.env.DB_NAME || 'PA24';
+const dbUser = process.env.DB_USER || 'username';
+const dbPassword = process.env.DB_PASSWORD || 'password';
+const dbHost = process.env.DB_HOST || 'localhost';
+const dbPort = Number(process.env.DB_PORT) || 5432;
+
+/**
+ * @class DatabaseConnection
+ * @description TODO
+ */
+class DatabaseConnection {
+  private static instance: DatabaseConnection;
+
+  private connection: Sequelize;
+
+  /**
+   * @constructor
+   * @description TODO
+   */
+  private constructor() {
+    this.connection = new Sequelize(dbName, dbUser, dbPassword, {
+      host: dbHost,
+      port: dbPort,
+      dialect: 'postgres',
+      logging: false, // Disabilita i log delle query SQL
+    });
+  }
+
+  /**
+   * @function getInstance
+   * @description Ritorna l'istanza singleton della connessione al database.
+   * @returns {Sequelize} L'istanza Sequelize della connessione al database.
+   */
+  public static getInstance(): Sequelize {
+    if (!DatabaseConnection.instance) {
+      DatabaseConnection.instance = new DatabaseConnection();
+    }
+    return DatabaseConnection.instance.connection;
+  }
+}
+
+export default DatabaseConnection;
